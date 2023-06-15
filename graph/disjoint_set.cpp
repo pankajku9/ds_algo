26d7099375b89bc27d71be01e5e94d1ce0299b3c
pankajku9 <32638492+pankajku9@users.noreply.github.com>
2023-06-15 19:10:58 +0000

    Basic disjoint set

---
+++ graph/disjoint_set.cpp
@@ -0,0 +1,28 @@
+    #include <vector>
+
+    using namespace std;
+
+    void make_set(vector<int> &par)
+    {
+        for (int i = 0; i < par.size();i++)
+            par[i] = i;
+    }
+    
+    int set_find(vector<int> &par, int x)
+    {
+        if (par[x] == x)
+            return x;
+        return set_find(par, par[x]);
+    }
+
+    void set_union(vector<int> &par, int x, int y)
+    {
+        par[y] = x;
+    }
+
+    int main()
+    {
+        vector<int> par(3);
+        make_set(par);
+
+    }
